356634dd0a4bc5fc4bc15882f2b955dc9af6e143
Jeffry Steegmans <jeffrysteegmans@gmail.com>
2022-12-31 16:01:00 +0100

    Copy files from StartFolder to ResultFolder

---
+++ PhotoMover.CLI/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.Title = "PhotoMover";
+
+var _extensions = new List<string> { ".jpg", ".jpeg", ".png" };
+
+Console.Write("Start folder: ");
+var startFolder = Console.ReadLine();
+
+if (startFolder is null)
+{
+    Environment.Exit(1);
+}
+
+Console.Write("Results folder: ");
+var resultsFolder = Console.ReadLine();
+
+if (resultsFolder is null)
+{
+    Environment.Exit(1);
+}
+
+var photos = new List<string>();
+Console.WriteLine("Indexing photo's...");
+foreach (var subfolder in Directory.GetDirectories(startFolder))
+{
+    Console.Write($"Scanning folder '{subfolder}': ");
+    var photoCounter = 0;
+    foreach (var file in Directory.GetFiles(subfolder))
+    {
+        if (_extensions.Any(x => file.EndsWith(x, StringComparison.InvariantCultureIgnoreCase)))
+        {
+            photos.Add(file);
+            photoCounter++;
+        }
+    }
+    Console.WriteLine($"{photoCounter} photo's found in subfolder");
+}
+
+Console.WriteLine($"Found {photos.Count} photo's.");
+
+Console.WriteLine($"Copying photo files from {startFolder} to {resultsFolder}");
+
+var counter = 1;
+foreach (var photo in photos)
+{
+    File.Copy(photo,$@"{resultsFolder}\{counter:D4}.jpg");
+    counter++;
+}
